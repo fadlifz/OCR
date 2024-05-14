3b1151d560b7f1ed60ed7c808b54e11553a85b64
fadlifz <fadlifaturzaman@gmail.com>
2024-05-14 12:29:07 +0700

    change request body to request param

--- src/main/java/com/example/ocr/controller/FileScannerController.java
+++ src/main/java/com/example/ocr/controller/FileScannerController.java
@@ -1,14 +1,15 @@
 package com.example.ocr.controller;
 
-import com.example.ocr.request.FileRequest;
 import com.example.ocr.service.ExtractionService;
 import net.sourceforge.tess4j.TesseractException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -21,11 +22,17 @@ public class FileScannerController {
     private ExtractionService extractionService;
 
     @PostMapping("/extract-text")
-    ResponseEntity<Map<String, Object>> scanFile(@RequestBody FileRequest file)
+    ResponseEntity<Map<String, Object>> scanFile(@RequestHeader("Authorization") String authorization,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("requestId") String requestId)
             throws TesseractException, IOException {
-        String text = extractionService.extract(file);
         Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("extractedText", text);
-        return new ResponseEntity<>(jsonMap, HttpStatus.OK);
+        if (!authorization.isEmpty()) {
+            String text = extractionService.extract(file, requestId);
+            jsonMap.put("extractedText", text);
+            return new ResponseEntity<>(jsonMap, HttpStatus.OK);
+        }
+        jsonMap.put("extractedText", null);
+        return new ResponseEntity<>(jsonMap, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/example/ocr/service/ExtractionService.java
+++ src/main/java/com/example/ocr/service/ExtractionService.java
@@ -4,59 +4,36 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 import org.springframework.stereotype.Service;
-
-import com.example.ocr.request.FileRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.ByteArrayInputStream;
 import java.io.File;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import javax.xml.bind.DatatypeConverter;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.FileOutputStream;
 
 @Service
 public class ExtractionService {
 
-    public static final DateTimeFormatter yyMMddHHmmssSSFormat = DateTimeFormatter.ofPattern("yyMMddHHmmssSS");
+    public String extract(MultipartFile file, String requestId)
+            throws IOException, TesseractException {
 
-    public String extract(FileRequest file) throws IOException, TesseractException {
-
-        String[] strings = file.getFile().split(",");
-        String extension;
-        switch (strings[0]) {// check image's extension
-            case "data:image/jpeg;base64":
-                extension = "jpeg";
-                break;
-            case "data:image/png;base64":
-                extension = "png";
-                break;
-            default:// should write cases for more images types
-                extension = "jpg";
-                break;
+        if (!requestId.isEmpty()) {
+            Tesseract tesseract = new Tesseract();
+            tesseract.setDatapath("src/main/resources/");
+            tesseract.setLanguage("eng");
+            String convertImage = tesseract.doOCR(convert(file));
+            String output = convertImage.replaceAll("\\\n", " ");
+            output = output.replaceAll(" ", " ");
+            return output;
         }
-        // convert base64 string to binary data
-        BufferedImage image = null;
-        byte[] data = DatatypeConverter.parseBase64Binary(strings[1]);
-        ByteArrayInputStream bis = new ByteArrayInputStream(data);
+        return "";
+    }
 
-        image = ImageIO.read(bis);
-        bis.close();
-
-        // write the image to a file
-        File outputFile = new File("image_" + yyMMddHHmmssSSFormat.format(LocalDateTime.now()) + "." + extension);
-        ImageIO.write(image, extension, outputFile);
-
-        Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("src/main/resources/");
-        tesseract.setLanguage("eng");
-        String convertImage = tesseract.doOCR(outputFile);
-        outputFile.delete();
-        String output = convertImage.replaceAll("\\\n", " ");
-        output = output.replaceAll(" ", " ");
-        return output;
+    public static File convert(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        convFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
     }
 }
